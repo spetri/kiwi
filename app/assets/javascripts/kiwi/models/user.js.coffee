bc26b471940fe789f89dbc0d5e131167ba1110a6
Jon Lebensold <jon@lebensold.ca>
2013-09-24 09:53:19 +0200

    touching up the facebook signin

--- app/assets/javascripts/kiwi/models/user.js.coffee
+++ app/assets/javascripts/kiwi/models/user.js.coffee
@@ -2,3 +2,4 @@ class FK.Models.User extends Backbone.Model
   idAttribute: "_id"
   defaults:
     email: ''
+    provider: ''
--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -14,15 +14,12 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
   
   def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    pp env
-#    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
+    @user = User.from_omniauth(auth)
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.twitter_uid"] = auth["uid"]
+      session["devise.facebook_uid"] = auth["uid"]
       redirect_to new_user_registration_url
     end
   end
