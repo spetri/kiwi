d0a2f96b889e49d20c7fcb3a62cb875d54c01ded
Jon Lebensold <jon@lebensold.ca>
2013-09-24 09:05:39 +0200

    Issue #25 added startup script for handling keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development? 
-  provider :twitter, "912tZV4jxlwOlwNuISVGpA", "ShyES9pxFEkziVPbQwcisjQTFunH2kyP956YXE"
-  provider :facebook, "170557669803798", "adc0f4dbb83cd185ae05d532d2d8c431", :scope => 'email'
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'] 
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'] , :scope => 'email'
 end
