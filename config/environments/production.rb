614032751dfa2f1edd206f84d3dead23b77e7004
Grayden Smith <grayden@tech-vision.ca>
2014-03-22 14:42:23 +0000

    Issue #78 Configure staging and production email
    Add configuration to staging and production files for email settings.
    Configure devise address to be hello@forekast.com.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Kiwi::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Mailer
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :domain         => 'gmail.com',
+    :authentication => :plain,
+    :user_name      => 'hello@forekast.com',
+    :password       => ENV['EMAIL_PASSWORD']
+  }
 end
