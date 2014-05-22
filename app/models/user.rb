c53917f74a701f91b26a724fde6df6c0fc4f15b7
Grayden Smith <grayden@tech-vision.ca>
2014-05-22 09:55:35 -0400

    [#71702552] Introduce moderators
    Add a property to the user that can identify it as a moderator.
    Add a rake task for making users into moderators.

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,6 +55,8 @@ class User
   field :country,   :type => String
   field :subkasts,  :type => Array
 
+  field :moderator, :type => Boolean
+
   include Mongoid::Timestamps
 
   def login=(login)
@@ -104,6 +106,10 @@ class User
 
   def email_required?
     super && provider.blank?
+  end
+
+  def moderator?
+    moderator
   end
 
   def admin?
