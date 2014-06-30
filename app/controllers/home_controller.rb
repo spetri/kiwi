7f42d791f86b76533d07eba9c167e6029c955b98
Grayden Smith <grayden@tech-vision.ca>
2014-06-29 21:47:08 -0400

    [#73023496] Rename and create subkasts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,4 @@
 class HomeController < ApplicationController
   def index
-    @countries = Country.all.sort_by(&:en_name).to_json
-    @all_subkasts = Subkast.all.to_json
-    @my_subkasts = Subkast.in(code: current_user.my_subkasts).to_json
   end
 end
