8f0f8caacd065a9ad0f30b9d1607c60bee2f420f
Grayden Smith <grayden@tech-vision.ca>
2014-07-11 22:46:50 -0400

    [#70761980] Add test for ignoring comments

--- spec/mailers/comment_mailer_spec.rb
+++ spec/mailers/comment_mailer_spec.rb
@@ -4,13 +4,22 @@ describe CommentMailer do
   let(:user1) { create :user }
   let(:user2) { create :user }
   let(:user3) { create :user }
+  let(:user4) { _user = create :user
+    _user.receive_comment_notifications = false 
+    _user.save
+    return _user
+  }
 
   let(:event1) { create :event, user: user1.username }
+  let(:event2) { create :event, user: user4.username }
   
   let(:comment1) { create :comment, event: event1, authored_by: user2, parent: nil }
   let(:comment2) { create :comment, event: event1, authored_by: user1, parent: nil }
-  let(:reply1) { create :comment, event: event1, authored_by: user3, parent: comment1  }
-  let(:reply2) { create :comment, event: event1, authored_by: user3, parent: comment2  }
+  let(:comment3) { create :comment, event: event2, authored_by: user1, parent: nil }
+  let(:comment4) { create :comment, event: event1, authored_by: user4, parent: nil }
+  let(:reply1) { create :comment, event: event1, authored_by: user3, parent: comment1 }
+  let(:reply2) { create :comment, event: event1, authored_by: user3, parent: comment2 }
+  let(:reply3) { create :comment, event: event1, authored_by: user2, parent: comment4 }
 
   before(:each) do
     ActionMailer::Base.deliveries.reject! { |d| true }
@@ -34,5 +43,15 @@ describe CommentMailer do
   it 'should not send a notice when the event owner is also the commenter' do
     CommentMailer.send_notifications(comment2)
     ActionMailer::Base.deliveries.count.should == 0
+  end
+
+  it 'should not send notifications when the event owner has chosen to ignore notifications' do
+    CommentMailer.send_notifications(comment3)
+    ActionMailer::Base.deliveries.count.should == 0
+  end
+
+  it 'should not send notifications when the comment owner has chosen to ignore notifications' do
+    CommentMailer.send_notifications(reply3)
+    ActionMailer::Base.deliveries.count.should == 1
   end
 end
